926f5314d306f8ae0510e29947e6e6ef9b352ad0
ooepi <robert.harmaakivi@gmail.com>
2021-01-31 23:09:31 +0200

    Finished with all extended classes

---
+++ Pigeon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Birds
+{
+    class Pigeon : Bird
+    {
+        public override Egg[] LayEggs(int numberOfEggs)
+        {
+            Egg[] eggs = new Egg[numberOfEggs];
+
+            for (int i = 0; i < numberOfEggs; i++)
+            {
+                eggs[i] = new Egg((Randomizer.Next(1, 3) + Randomizer.NextDouble()), "white");
+            }
+            return eggs;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,39 @@ namespace Birds
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Bird bird;
+                Console.Write("\nPress P for pigeon, O for ostrich: ");
+
+                char key = Char.ToUpper(Console.ReadKey().KeyChar);
+
+                if (key == 'P')
+                {
+                    bird = new Pigeon();
+                }
+                else if (key == 'O')
+                {
+                    bird = new Ostrich();
+                }
+                else
+                {
+                    return;
+                }
+
+                Console.WriteLine("\nHow many eggs should it lay? ");
+
+                if (!int.TryParse(Console.ReadLine(), out int numberOfEggs))
+                {
+                    return;
+                }
+                Egg[] eggs = bird.LayEggs(numberOfEggs);
+
+                foreach (Egg egg in eggs)
+                {
+                    Console.WriteLine(egg.Description);
+                }
+            }
         }
     }
 }
