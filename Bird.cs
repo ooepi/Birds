8e36192bb1622f5f181ba63da9052a67ac86cf18
ooepi <robert.harmaakivi@gmail.com>
2021-02-11 23:40:11 +0200

    Changed the class to abstract

--- Bird.cs
+++ Bird.cs
@@ -6,14 +6,10 @@ using System.Threading.Tasks;
 
 namespace Birds
 {
-    class Bird
+    abstract class Bird
     {
         public static Random Randomizer = new Random();
 
-        public virtual Egg[] LayEggs(int numberOfEggs)
-        {
-            Console.Error.WriteLine("Bird.LayEggs should never get called");
-            return new Egg[0];
-        }
+        public abstract Egg[] LayEggs(int numberOfEggs);
     }
 }
